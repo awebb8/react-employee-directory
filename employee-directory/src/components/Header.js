359e3afa80d763a84e68023d5d3f2bb18215762b
Alkawe <alixkwebb@gmail.com>
2020-10-27 22:03:01 -0400

    added Header file which adds navbar

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/Header.css";
+
+function Header() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <span className="navbar-brand mb-0 h1">Employee Directory</span>
+        </nav>
+    );
+}
+
+export default Header;
