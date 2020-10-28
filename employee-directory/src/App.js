712232aabb1148d4b74389d574a084e672b2b2a2
Alkawe <alixkwebb@gmail.com>
2020-10-27 22:14:44 -0400

    added App function which returns the Header and EmployeeTable

---
+++ employee-directory/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import EmployeeTable from './components/EmployeeTable'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <EmployeeTable/>
+    </>
+  );
+}
+
+export default App;
