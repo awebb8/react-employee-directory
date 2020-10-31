ce27ef0f8acc0763a6460d6c4bacf94d8800bf7e
Alkawe <alixkwebb@gmail.com>
2020-10-31 11:49:18 -0400

    modified styling

--- employee-directory/src/components/Header.js
+++ employee-directory/src/components/Header.js
@@ -3,9 +3,10 @@ import "../style/Header.css";
 
 function Header() {
     return (
-        <nav className="navbar navbar-dark bg-dark">
+        <nav className="navbar justify-content-center header">
             <span className="navbar-brand mb-0 h1">Employee Directory</span>
         </nav>
+
     );
 }
 
